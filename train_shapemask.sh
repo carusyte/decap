81fc44f9c3b42aa7d782da1058b7da0cefb3bf3e
Jonathan <carusyte@163.com>
2021-08-30 15:19:15 +0800

    added shapemask training script

---
+++ train_shapemask.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+MODEL_DIR="model"
+python3 model_main_tf2.py \
+  --strategy_type=one_device \
+  --num_gpus=1 \
+  --model_dir="${MODEL_DIR?}" \
+  --mode=train \
+  --config_file="config/shapemask.yml"
